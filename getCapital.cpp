b8fc36293225a27af22152ab685d487d9f4ef81b
Kebin Yan <yankebin99@gmail.com>
2024-09-13 16:15:29 -0400

    create getCapital program

---
+++ getCapital.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+    		fprintf(stderr, "Error: No country provided.\n");
+    		exit(127);
+  	}
+	
+	std::string country = argv[1];
+	if (country == "USA") {
+		std::cout << "Washington D.C." << std::endl;
+	} else {
+		std::cout << "I don't the capital for " << country << std::endl;
+	}
+}
